a838e270c390d93d42fe80d314440937fefcb0ac
Ansy-13 <aimahadia13@gmail.com>
2024-04-08 16:55:25 +0500

    push my project to github

---
+++ main.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const input = await inquirer.prompt([
+    { message: "enter user name", type: "string", name: "userName" },
+    { message: "enter password", type: "number", name: "password" }
+]);
+let userName = "anas";
+let password = 1234;
+if (input.userName === "anas" && input.password === 1234) {
+    console.log("you login succesfully");
+}
+else {
+    console.log("sorry you cant login");
+}
